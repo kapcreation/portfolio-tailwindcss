b69c68f461aae65567735a796844dba1b4fc3ff1
kap <kgdee7603@gmail.com>
2023-02-22 04:56:58 +0800

    commit

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,11 +1,22 @@
 // Fixed navbar
-this.window.addEventListener("scroll", function() {
+window.addEventListener("scroll", function() {
   const header = document.querySelector('#header')
 
-  if (this.window.scrollY > header.offsetTop) {
+  if (window.scrollY > header.offsetTop) {
     header.classList.add("nav-fixed")
   } else {
     header.classList.remove("nav-fixed")
+  }
+})
+
+// Scrolltop
+window.addEventListener("scroll", function() {
+  const scrolltop = document.querySelector("#scrolltop")
+
+  if (window.scrollY > 500) {
+    scrolltop.classList.add("scrolltop-show")
+  } else {
+    scrolltop.classList.remove("scrolltop-show")
   }
 })
 
@@ -14,12 +25,53 @@ this.window.addEventListener("scroll", function() {
 const hamburger = document.querySelector("#hamburger")
 const navMenu = document.querySelector("#nav-menu")
 
-hamburger.addEventListener("click", function() {
+function toggleMenu() {
   navMenu.classList.add("menu-transition")
   hamburger.classList.toggle("active")
   navMenu.classList.toggle("menu-close")
-})
+}
+
+hamburger.addEventListener("click", toggleMenu)
 
 this.window.addEventListener("resize", function() {
   navMenu.classList.remove("menu-transition")
+})
+
+
+
+// Dark mode
+const darkMode = () => (JSON.parse(localStorage.darkMode) || false)
+const darkmodeToggle = document.querySelector("#darkmode-toggle")
+
+if (darkMode()) {
+  document.documentElement.classList.add('dark')
+  darkmodeToggle.innerHTML = `<i class="bi bi-brightness-high-fill"></i>`
+} else {
+  document.documentElement.classList.remove('dark')
+  darkmodeToggle.innerHTML = `<i class="bi bi-moon"></i>`
+}
+
+darkmodeToggle.addEventListener("click", function () {
+  if (darkMode()) {
+    document.documentElement.classList.remove('dark')
+    darkmodeToggle.innerHTML = `<i class="bi bi-moon"></i>`
+  } else {
+    document.documentElement.classList.add('dark')
+    darkmodeToggle.innerHTML = `<i class="bi bi-brightness-high-fill"></i>`
+  }
+
+  localStorage.darkMode = !darkMode()
+})
+
+
+
+// Mobile nav menu
+const navMenuLinks = document.querySelectorAll(".nav-menu__link")
+
+navMenuLinks.forEach(link => {
+  link.addEventListener("click", function () {
+    navMenu.classList.add("menu-transition")
+    hamburger.classList.remove("active")
+    navMenu.classList.add("menu-close")
+  })
 })
